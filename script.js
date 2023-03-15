2dcfc220040841c99d3842879d1cf3ead8d1272c
Agustin Puig <apuig137@gmail.com>
2023-03-15 18:37:36 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,33 @@
+let button = document.querySelector("button")
+let container = document.querySelector(".container")
+let error = document.querySelector(".error")
+
+let cargarCiudad = (ciudadElegida) => {
+    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${ciudadElegida}&appid=7f5482804e804c168f596be3f1c6ed56`)
+        .then( (resp) => resp.json() )
+        .then( (data) => {
+            if(data.cod === "404"){
+                console.log("Ciudad no encontrada")
+                container.style.visibility="hidden"
+                error.style.display = "flex"
+            } else {
+                console.log(data)
+                let {name, main:{temp, temp_min, temp_max}, weather:[arr]} = data
+                error.style.display = "none"
+                container.style.visibility = "visible"
+                document.querySelector("#ciudad").textContent = name
+                document.querySelector("#temperatura").textContent = parseInt(temp-273.15) + "°C"
+                document.querySelector("#wicon").src = `https://openweathermap.org/img/wn/${arr.icon}@2x.png`
+                document.querySelector("#descripcion").textContent = arr.description
+                document.querySelector("#max-min").textContent = parseInt(temp_max-273.15) + "°C / " + parseInt(temp_min-273.15) + "°C"
+            }
+            
+        } )
+}
+
+button.addEventListener("click",() => {
+    let input = document.querySelector("input")
+    let ciudad = input.value
+    cargarCiudad(ciudad)
+})
+
