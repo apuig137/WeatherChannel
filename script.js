0547f93202c114f39e658ee0c3d5130b22cc8c86
Agustin Puig <apuig137@gmail.com>
2023-04-16 16:23:28 -0300

    country

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 let button = document.querySelector("button")
 let container = document.querySelector(".container")
 let error = document.querySelector(".error")
+let pais
 
 
-
-let cargarCiudad = (ciudadElegida) => {
-    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${ciudadElegida}&lang=sp&appid=7f5482804e804c168f596be3f1c6ed56`)
+let cargarCiudad = async (ciudadElegida) => {
+    await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${ciudadElegida}&lang=sp&appid=7f5482804e804c168f596be3f1c6ed56`)
         .then( (resp) => resp.json() )
         .then( (data) => {
             if(data.cod === "404"){
@@ -15,28 +15,26 @@ let cargarCiudad = (ciudadElegida) => {
             } else {
                 console.log(data)
                 let {name, main:{temp, temp_min, temp_max}, weather:[arr], sys:{country}} = data
+                pais = country
                 error.style.display = "none"
                 container.style.visibility = "visible"
-                document.querySelector("#ciudad").textContent = `${name}, ${country}`
+                document.querySelector("#ciudad").textContent = `${name}, `
                 document.querySelector("#temperatura").textContent = parseInt(temp-273.15) + "°C"
                 document.querySelector("#wicon").src = `https://openweathermap.org/img/wn/${arr.icon}@2x.png`
                 document.querySelector("#descripcion").textContent = arr.description
                 document.querySelector("#max-min").textContent = parseInt(temp_max-273.15) + "°C / " + parseInt(temp_min-273.15) + "°C"
             }
         } )
+    fetch(`https://restcountries.com/v3.1/alpha/${pais}`)
+        .then((resp) => {
+            return resp.json()
+        })
+        .then( (data) => {
+            console.log(data[0].name.common)
+            document.querySelector("#pais").textContent = data[0].name.common
+            return data[0].name.common
+        })
 }
-
-//let cargarPais = (cod) => {
-//    fetch(`https://restcountries.com/v3.1/alpha/${cod}`)
-//        .then((resp, data) => {
-//            resp.json()
-//        })
-//        .then( (data) => {
-//            console.log(data[0].name.common)
-//            return data[0].name.common
-//        })
-//}
-
 
 button.addEventListener("click",() => {
     let input = document.querySelector("input")
